c7ad0190c2d766934ece4ad7a89ec0278b3e2ce0
KIMBORA <rabo93@gmail.com>
2024-11-05 15:40:45 +0900

    HomeController 주석추가

--- Git_Test_KBR/src/main/java/com/itwillbs/gittest_kbr/HomeController.java
+++ Git_Test_KBR/src/main/java/com/itwillbs/gittest_kbr/HomeController.java
@@ -19,6 +19,7 @@ public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		//index 페이지 포워딩
 		return "index";
 	}
 	
