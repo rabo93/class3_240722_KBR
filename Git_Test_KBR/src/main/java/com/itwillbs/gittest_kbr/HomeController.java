990261d869e23d09d28bd91657f8be171cefc4bf
KIMBORA <rabo93@gmail.com>
2024-11-05 17:10:47 +0900

    원복 하겟습니당

--- Git_Test_KBR/src/main/java/com/itwillbs/gittest_kbr/HomeController.java
+++ Git_Test_KBR/src/main/java/com/itwillbs/gittest_kbr/HomeController.java
@@ -8,8 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//lllll
-//아무거나
+
 /**
  * Handles requests for the application home page.
  */
